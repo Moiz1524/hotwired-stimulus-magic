3fa73e5a5ae3a8a530981c502f1c90ceb723be46
Moiz Ali <amoiz4142@gmail.com>
2023-02-02 18:57:26 +0500

    Add hotwire broadcasts & some attrs

--- db/migrate/20220907092439_create_events.rb
+++ db/migrate/20220907092439_create_events.rb
@@ -2,6 +2,8 @@ class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
       t.string :name
+      t.integer :likes_count, default: 0
+      t.integer :reposts_count, default: 0
 
       t.timestamps
     end
